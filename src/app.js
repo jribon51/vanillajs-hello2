1e505f001e13f9c89aaa14ab287070996e69660d
jribon51 <151412716+jribon51@users.noreply.github.com>
2024-02-14 05:00:04 +0000

    entrega 2 java script

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let variable =
+    who[Math.floor(Math.random() * 4)] +
+    " " +
+    action[Math.floor(Math.random() * 4)] +
+    " " +
+    what[Math.floor(Math.random() * 3)] +
+    " " +
+    when[Math.floor(Math.random() * 5)];
+  document.getElementById("excuse").innerHTML = variable;
 };
